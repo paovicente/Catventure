5ddfe87b233baa8c9dc8d196f68da377f3359f0c
Paola Vicente <paovicente1297@gmail.com>
2025-09-17 17:55:46 -0300

    Some tilemaps added
    I added some tilemaps for sky, plaforms, floor and configured them in the corresponding Layer, Sorting Layer and Order In Layer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,36 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionReference moveAction;
-    [SerializeField] private GameObject blackScreen;
-    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private InputActionReference jumpAction;
     private Vector2 moveInput;
+    private float jumpInput;
+    [SerializeField] private float playerSpeed = 5f;
+    private bool isJumping = false;
 
+    [SerializeField] private GameObject blackScreen;
+    
     private void Update()
     {
         if (!blackScreen.activeSelf) { //when the black start screen is disabled the player can move
             moveAction.action.started += HandleMoveInput;
             moveAction.action.performed += HandleMoveInput;
             moveAction.action.canceled += HandleMoveInput;
+            MovePlayer();
 
-            movePlayer();
+            if (gameObject.transform.position.y < 0) /*if the cat is on the floor - only to test, it will be changed once 
+                                                       * the tilemaps are correctly configured*/
+            {
+                isJumping = false;
+            }
+
+            if (isJumping == false){//to block a double jump
+                jumpAction.action.started += HandleJumpInput;
+                jumpAction.action.performed += HandleJumpInput;
+                jumpAction.action.canceled += HandleJumpInput;
+                Jump();
+            }
+
+            
             Debug.Log("black screen inactive");
         }
         else
@@ -30,11 +48,22 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
     }
-   
-    private void movePlayer()
+
+    private void HandleJumpInput(InputAction.CallbackContext context)
+    {
+        jumpInput = context.ReadValue<float>();
+    }
+
+    private void MovePlayer()
     {
         Vector3 move = new Vector3(moveInput.x, moveInput.y);
 
         transform.position += move * playerSpeed * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        isJumping = true;
+        Debug.Log("Esta saltando");
     }
 }
